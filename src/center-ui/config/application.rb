aba05b20760e03d492aab25bbaaa487ea93be36c
Fernando Santos <nanndoj@gmail.com>
2023-03-13 16:43:39 +0100

    feat: translate center-ui to pt-BR

--- src/center-ui/config/application.rb
+++ src/center-ui/config/application.rb
@@ -59,7 +59,9 @@ module CenterUi
 
     # The default locale is :et and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = :en
+    config.i18n.enforce_available_locales = false
+    config.i18n.available_locales = ["pt-BR"]
+    config.i18n.default_locale = :'pt-BR'
     config.i18n.fallbacks = true
 
     # Configure the default encoding used in templates for Ruby 1.9.
